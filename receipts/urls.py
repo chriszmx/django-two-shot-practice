94f8603580dfb43425da5f8ce1d8637d6c8209a3
Chris <c.r.zambito@gmail.com>
2023-03-05 14:28:28 -0500

    added feature 12

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from receipts.views import (receipt,
-                            )
+                            create_receipt,
+                            category_list,
+                            account_list)
 
 
 urlpatterns = [
+    path('accounts/', account_list, name='account_list'),
+    path('categories/', category_list, name='category_list'),
+    path('create/', create_receipt, name='create_receipt'),
     path('', receipt, name='home'),
 ]
-
-
--- receipts/views.py
+++ receipts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from receipts.models import Receipt
+from receipts.models import Receipt, ExpenseCategory,  Account
+from receipts.forms import  ReceiptForm
 
 
 # Create your views here.
@@ -11,3 +12,38 @@ def receipt(request):
         'receipts': receipts,
     }
     return render(request, 'receipts/receipt.html', context)
+
+
+@login_required
+def create_receipt(request):
+    if request.method == 'POST':
+        form = ReceiptForm(request.POST)
+        if form.is_valid():
+            receipt = form.save(commit=False)
+            receipt.purchaser = request.user
+            receipt.save()
+            return redirect('home')
+    else:
+        form = ReceiptForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'receipts/create_receipt.html', context)
+
+
+@login_required
+def category_list(request):
+    categories = ExpenseCategory.objects.filter(owner=request.user)
+    context = {
+        'receipts': categories,
+    }
+    return render(request, 'receipts/category_list.html', context)
+
+
+@login_required
+def account_list(request):
+    accounts = Account.objects.filter(owner=request.user)
+    context = {
+        'receipts': accounts,
+    }
+    return render(request, 'receipts/account_list.html', context)
